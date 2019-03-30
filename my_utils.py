dfcd8a9309a1a6785cf1f7c4debbeeffc4c1b353
DianaPredoiu <diana.predoiu95@yahoo.com>
2019-03-30 13:58:35 +0200

    sound can be splitted now in 20ms or less without being slow motioned

--- my_utils.py
+++ my_utils.py
@@ -24,7 +24,7 @@ def mix_audios(filename1, filename2):
 
     sound1_start, sound1_end = filename1.index('arctic'), filename1.index('.wav')
     sound2_start, sound2_end = filename2.index('arctic'), filename2.index('.wav')
-    name = "../recordings/mixes/" + filename1[sound1_start:sound1_end] + '_' + filename2[sound2_start:sound2_end] + ".wav"
+    name = "../../recordings/mixes/" + filename1[sound1_start:sound1_end] + '_' + filename2[sound2_start:sound2_end] + ".wav"
     combined.export(name, format='wav')
     
     return name
@@ -96,7 +96,7 @@ def get_stft_matrix_from_mixture(mask, mixture):
 
 
 def write_new_audio_file(sound, filename):
-    inverse_sound_stft = librosa.istft(sound, , hop_length=512)
+    inverse_sound_stft = librosa.istft(sound)
     librosa.output.write_wav(filename, inverse_sound_stft, 16000)
     #s = sound.export(filename, format="wav")
 
